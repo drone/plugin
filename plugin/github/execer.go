cc4dd149e5a6315b059f71cf4a2b821b9a4abe44
Devansh Mathur <devanshmathur@harness.io>
2025-04-21 20:56:11 +0530

    Adding support for Node20 using FF.

--- plugin/github/execer.go
+++ plugin/github/execer.go
@@ -13,7 +13,8 @@ import (
 	"path/filepath"
 
 	"github.com/drone/plugin/plugin/internal/environ"
-	"github.com/nektos/act/cmd"
+	v1 "github.com/harness/nektos-act/cmd"
+	v2 "github.com/harness/nektos-act/v2/cmd"
 	"github.com/pkg/errors"
 	"golang.org/x/exp/slog"
 )
@@ -82,7 +83,15 @@ func (e *Execer) Exec(ctx context.Context) error {
 		os.Args = append(os.Args, "--env", k+"="+v)
 	}
 
-	cmd.Execute(ctx, "1.1")
+	// Check if the USE_NEKTOS_ACT_NODE_20 environment variable is set to true
+	slog.Info("Checking value of env var USE_NEKTOS_ACT_NODE_20", "value", os.Getenv("USE_NEKTOS_ACT_NODE_20"))
+	if os.Getenv("USE_NEKTOS_ACT_NODE_20") == "true" {
+		slog.Info("Using nektos-act v2 for execution")
+		v2.Execute(ctx, "1.1")
+	} else {
+		slog.Info("Using nektos-act v1 for execution")
+		v1.Execute(ctx, "1.1")
+	}
 
 	if err := exportEnv(beforeStepEnvFile, afterStepEnvFile); err != nil {
 		return err
